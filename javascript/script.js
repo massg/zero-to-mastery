864b9d663de4e5fb4e0ffa090b3a002d5cab97e2
massg <sreesruthipk@gmail.com>
2020-06-26 10:55:29 +0530

    functions

--- javascript/script.js
+++ javascript/script.js
@@ -1,2 +1,21 @@
-  alert("Welcome to the site!!");
+function sayHello(){
   console.log("Hello");
+}
+var sayBye = function(){
+  console.log("Bye");
+}
+function sing(song){
+  console.log(song);
+}
+
+
+
+
+
+
+
+alert("Welcome to the site!!");
+sayHello();
+sing("LALALA");
+sing("Bababa")
+sayBye();
