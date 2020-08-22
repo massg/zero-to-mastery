9ce5fef4afa0593cb32aa1c48742b7fc41c0172e
massg <sreesruthipk@gmail.com>
2020-08-22 21:26:16 +0530

    bg generator

--- DOM manipulation/bg/script.js
+++ DOM manipulation/bg/script.js
@@ -1,3 +1,17 @@
 var c = document.querySelector("h3");
 var c1 = document.querySelector(".c1");
 var c2 = document.querySelector(".c2");
+var body = document.getElementById("full");
+
+function setGradient(){
+  body.style.background = "linear-gradient(to right,"
+                            +c1.value
+                            +","
+                            +c2.value
+                            +")"
+
+   c.textContent = body.style.background+ ";";
+}
+
+c1.addEventListener("input",setGradient);
+c2.addEventListener("input",setGradient);
